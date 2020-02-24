1acaa77f86faa2f587f65247a74026f3252f4ef3
Nadya <vip.kovcunyak60@gmail.com>
2020-02-24 11:06:22 +0200

    Додана консоль

---
+++ ПР2/ClassLibrary1/Student.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   public class Student
+    {
+        public string Name { set; get; }
+        public string Surname { set; get; }
+        public string Group { set; get; }
+        public int Year { set; get; }
+        public double Price { set; get; }
+        public string PaymentTime { set; get; }
+        public Result[] Results { set; get; }
+        public Student() { }
+        public Student(string name, string surname, string group, int year, Result[] results, double price, string paymentTime)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Year = year;
+            Price = price;
+            PaymentTime = paymentTime;
+            Results = new Result[results.Count()];
+            for (int i = 0; i < results.Count(); i++)
+            {
+                Results[i] = results[i];
+            }
+        }
+
+        //calculates the arithmetic mean of all estimates
+        public double GetAveragePoints()
+        {
+            int sum = 0;
+            for (int i = 0; i < Results.Count(); i++)
+            {
+                sum += Results[i].Points;
+            }
+            return (Double)sum / Results.Count();
+        }
+
+        //returns the name of the subject by which the student has the highest score among other subjects
+        public string GetBestSubject()
+        {
+            int MaxPoint = 0;
+            int MaxIndex = 0;
+            for (int i = 0; i < Results.Count(); i++)
+            {
+                if (Results[i].Points > MaxPoint)
+                {
+                    MaxPoint = Results[i].Points;
+                    MaxIndex = i;
+                }
+            }
+            return Results[MaxIndex].Subject;
+        }
+
+        //returns the name of the subject that gave the student the worst score.
+        public string GetWorstSubject()
+        {
+            int MinPoint = 101;
+            int MinIndex = 0;
+            for (int i = 0; i < Results.Count(); i++)
+            {
+                if (Results[i].Points < MinPoint)
+                {
+                    MinPoint = Results[i].Points;
+                    MinIndex = i;
+                }
+            }
+            return Results[MinIndex].Subject;
+        }
+
+        public void printPrice()
+        {
+            if (PaymentTime == "month")
+            {
+                Console.WriteLine($"Monthly payment {Price}");
+                Console.WriteLine($"Payment for the year {Price * 12}");
+                Console.WriteLine($"For the entire period of payment {Price * 40}");
+            }
+            if (PaymentTime == "year")
+            {
+                Console.WriteLine($"Monthly payment {Price / 12}");
+                Console.WriteLine($"Payment for the year {Price}");
+                Console.WriteLine($"For the entire period of payment {Price / 12 * 40}");
+            }
+            if (PaymentTime == "all of time")
+            {
+                Console.WriteLine($"Monthly payment {Price / 40}");
+                Console.WriteLine($"Payment for the year {Price / 40 * 12}");
+                Console.WriteLine($"For the entire period of payment {Price}");
+            }
+        }
+    }
+}
---
+++ ПР2/ПР2/Program (2).cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ПР2
+{
+    class Program
+    {
+
+        //reads data from the keyboard and returns an array of Student-type objects
+        static Student[] ReadStudentsArray()
+        {
+            string subject, teacher;
+            int points;
+            string name, surname, group;
+            int year;
+            double price;
+            string paymentTime;
+            
+            int exam;
+
+            Console.WriteLine("Enter the number of students: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Student[] students = new Student[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Student {i + 1}");
+                Console.WriteLine("Enter a student name: ");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Enter the student's last name: ");
+                surname = Console.ReadLine();
+
+                Console.WriteLine("Enter the student group code: ");
+                group = Console.ReadLine();
+
+                Console.WriteLine("Enter the student's course number: ");
+                year = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("How many exams did the student take? ");
+                exam = Convert.ToInt32(Console.ReadLine());
+
+                Result[] result = new Result[exam];
+                for (int j = 0; j < exam; j++)
+                {
+                    Console.WriteLine($"Subject {j + 1}");
+                    Console.WriteLine("The name of the subject: ");
+                    subject = Console.ReadLine();
+                    Console.WriteLine("Name of the teacher: ");
+                    teacher = Console.ReadLine();
+                    Console.WriteLine("Rating: ");
+                    points = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    result[j] = new Result(subject, teacher, points);
+                }
+                Console.WriteLine("For what period the payment will be made? (1 - month, 2 - year, 3 - whole time)");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter sum: ");
+                price = Convert.ToDouble(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        paymentTime = "month";
+                        break;
+                    case 2:
+                        paymentTime = "year";
+                        break;
+                    case 3:
+                        paymentTime = "all of time";
+                        break;
+                    default:
+                        Console.WriteLine("This choise doesn't exists!");
+                        i--;
+                        continue;
+                }
+
+                students[i] = new Student(name, surname, group, year, result, price, paymentTime);
+            }
+            return students;
+        }
+
+        //accepts a Student object and displays it
+        static void PrintStudent(Student stude)
+        {
+            Console.WriteLine($"Name: {stude.Name}");
+            Console.WriteLine($"Surname: {stude.Surname}");
+            Console.WriteLine($"Group: {stude.Group}");
+            Console.WriteLine($"Course: {stude.Year}");
+            Console.WriteLine("Tuition fee: ");
+            stude.printPrice();
+
+            for( int i = 0; i < stude.Results.Count(); i++)
+            {
+                Console.WriteLine($"Subject {i + 1}");
+                Console.WriteLine($"Name of the subject: {stude.Results[i].Subject}");
+                Console.WriteLine($"Name of the teacher: {stude.Results[i].Teacher}");
+                Console.WriteLine($"Rating: {stude.Results[i].Points}");
+            }
+        }
+
+        //accepts an array of Student objects and displays it
+        static void PrintStudents(Student[] stude)
+        {
+            for(int i = 0; i < stude.Count(); i++)
+            {
+                Console.WriteLine($"Name: {stude[i].Name}");
+                Console.WriteLine($"Surname: {stude[i].Surname}");
+                Console.WriteLine($"Group: {stude[i].Group}");
+                Console.WriteLine($"Course: {stude[i].Year}");
+                Console.WriteLine("Tuition fee: ");
+                stude[i].printPrice();
+
+                for (int j = 0; j < stude[i].Results.Count(); j++)
+                {
+                    Console.WriteLine($"Subject {j + 1}");
+                    Console.WriteLine($"Name of the subject: {stude[i].Results[j].Subject}");
+                    Console.WriteLine($"Name of the teacher: {stude[i].Results[j].Teacher}");
+                    Console.WriteLine($"Rating: {stude[i].Results[j].Points}");
+                }
+            }
+        }
+
+        // accepts an array of Student objects
+        // and returns the highest grade point average and the lowest grade point average through out-parameters
+        static void GetStudentsInfo(Student[] stude, out double MaxAveragePoint, out double MinAveragePoint)
+        {
+            MaxAveragePoint = 0;
+            MinAveragePoint = 101;
+
+            for(int i = 0; i < stude.Count(); i++)
+            {
+                if (stude[i].GetAveragePoints() > MaxAveragePoint) {
+                    MaxAveragePoint = stude[i].GetAveragePoints();
+                }
+
+                if (stude[i].GetAveragePoints() < MinAveragePoint)
+                {
+                    MinAveragePoint = stude[i].GetAveragePoints();
+                }
+            }
+        }
+
+        // takes an array of Student objects and sorts it by the student's grade point average
+        static void SortStudentsByPoints(Student[] stude)
+        {
+            Array.Sort(stude, (s1, s2) => s1.GetAveragePoints().CompareTo(s2.GetAveragePoints()));       
+        }
+
+        // takes an array of Student objects and sorts it by name
+        static void SortStudentsByName(Student[] stude)
+        {
+            Array.Sort(stude, (s1, s2) => s1.Surname.CompareTo(s2.Surname));
+        }
+
+        static void Main(string[] args)
+        {
+            bool isExit = false;
+            int n, s;
+            double MinPoint, MaxPoint;
+            Student[] stude = ReadStudentsArray();
+            while (!isExit)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1. Print information about one student.");
+                Console.WriteLine("2. Output information about all students.");
+                Console.WriteLine("3. Print the highest and lowest grade point average.");
+                Console.WriteLine("4. Sort students by grade point average.");
+                Console.WriteLine("5. Sort students by name.");
+                Console.WriteLine("6. Exit.");
+
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine($"Enter the student serial number: 0-{stude.Count() - 1} : ");
+                        s = Convert.ToInt32(Console.ReadLine());
+                        PrintStudent(stude[s]);
+                        break;
+                    case 2:
+                        PrintStudents(stude);
+                        break;
+                    case 3:
+                        GetStudentsInfo(stude, out MaxPoint, out MinPoint);
+                        Console.WriteLine($"Min point: {MinPoint}");
+                        Console.WriteLine($"Max Point: {MaxPoint}");
+                        break;
+                    case 4:
+                        SortStudentsByPoints(stude);
+                        break;
+                    case 5:
+                        SortStudentsByName(stude);
+                        break;
+                    case 6:
+                        isExit = true;
+                        break;
+                    default:
+                        Console.WriteLine("This case doesn't exists!");
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
